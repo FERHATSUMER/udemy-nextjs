cfb0f4bd70a65514f8c70fba25ef2276b98dc50d
FERHATSUMER <sumerf39@gmail.com>
2023-04-15 23:56:35 +0300

    udemy-nextjs

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,11 +1,19 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React from "react";
+import styles from "./styles.module.css";
+import Link from "next/link";
+import { AiOutlineMail } from "react-icons/ai";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <h4>Built with Next.js</h4>
+      <div className={styles.mail}>
+      <AiOutlineMail />
+      <Link href="mailto:sumerf39@gmail.com">
+        <h4>Send Mail</h4>
+      </Link>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- components/Header/index.js
+++ components/Header/index.js
@@ -1,26 +1,33 @@
 import React from "react";
 import Link from "next/link";
-import styles from './styles.module.css'
+import styles from "./styles.module.css";
+import { AiFillLinkedin } from "react-icons/ai";
+import { BsInstagram, BsGithub } from "react-icons/bs";
+
 const Header = () => {
   return (
     <header className={styles.header}>
-      <Link href="/" >
-        <img className={styles.profile}
+      <Link href="/">
+        <img
+          className={styles.profile}
           src="https://media.licdn.com/dms/image/D4D03AQFMfnp5H1M-vA/profile-displayphoto-shrink_800_800/0/1667210919849?e=2147483647&v=beta&t=SL-bp4f4k3ElkP3ot_ahz2KSGT-uH9OeKbtvYwBTajw"
           alt="Ferhat"
         />
       </Link>
-      <nav >
-      <div className={styles.about}>
-        <Link href="https://www.linkedin.com/in/ferhatsumer/" target="_blank">
+      <nav>
+        <div className={styles.about}>
+          <AiFillLinkedin />
+          <Link href="https://www.linkedin.com/in/ferhatsumer/" target="_blank">
             <h4>Linkedin</h4>
-        </Link>
-        <Link href="https://github.com/FERHATSUMER" target="_blank">
+          </Link>
+          <BsGithub />
+          <Link href="https://github.com/FERHATSUMER" target="_blank">
             <h4>GitHub</h4>
-        </Link>
-        <Link href="https://www.instagram.com/ferhatsumer_/" target="_blank">
-          <h4>Instagram</h4>  
-        </Link>
+          </Link>
+          <BsInstagram />
+          <Link href="https://www.instagram.com/ferhatsumer_/" target="_blank">
+            <h4>Instagram</h4>
+          </Link>
         </div>
       </nav>
     </header>
